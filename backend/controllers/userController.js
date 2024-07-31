bebfff9a0cf640dfd256bbc8628df9d6005303b8
dilasathapa <thapadilasa71@gmail.com>
2024-07-31 21:43:18 +0530

    added login logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,9 @@
 const bcrypt = require('bcryptjs')
-const User = require('../models/user.model')
+const User = require('../models/user.model');
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+
+const JWT_SECRET = process.env.JWT_SECRET
 
 const registerUser = async(req, res)=>{
     const {username, fullname, email, password} = req.body;
@@ -28,4 +32,27 @@ const registerUser = async(req, res)=>{
     }
 }
 
-module.exports = {registerUser}
+const loginUser = async(req, res)=>{
+    const {email, password} = req.body;
+    try {
+       const userexists = await User.findOne({where : email})
+       if(!userexists){
+            return res.status(401).json({error : 'User Not Found with Given email'})
+       }
+       const isMatching = bcrypt.compare(password, userexists.password)
+
+       if(!isMatching){
+        return res.status(401).json({error : 'Invalid password'})
+       }
+
+       
+       const token = jwt.sign({id: userexists.id, username : userexists.username}, JWT_SECRET, {expiresIn : '7d'})
+       res.status(200).json({token, message : 'Login Successfully'})
+
+    } catch (error) {
+        res.status(500).json({error : error.message})
+        console.log(error)
+    }
+}
+
+module.exports = {registerUser, loginUser}
